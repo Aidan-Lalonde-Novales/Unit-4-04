ab8f80d3c33770330c135e781c55b7dd39fd9a13
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-10 18:04:18 +0000

    fixed the variable thing lol

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Javascript that alerts the user what they entered into the text boxes.
 
-let fname = document.getElementById("fname").value;
-fname = 1;
-let lname = document.getElementById("lname").value;
-
 document.getElementById('submitbutton').addEventListener('click', myFunction)
 
 function myFunction () {
-  alert('Your name is '+fname+lname);
+  let fname = document.getElementById("fname").value;
+  let lname = document.getElementById("lname").value;
+  alert('Your name is '+fname+' '+lname);
 }
